6f2a2eee94b4de4000cb31f5d0b67c39f536a57c
Yuanyang Zhang <85085067+zhangyuanyang21@users.noreply.github.com>
2025-06-19 18:18:15 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -5,34 +5,27 @@ import scipy.io
 import torch
 import h5py
 
-class Mfeat():
+class UCI_digit():
     def __init__(self, path):
-
-        data = scipy.io.loadmat(path + 'Mfeat.mat')
         scaler = MinMaxScaler()
-        self.Y = data['truelabel'][0][0].astype(np.int32).reshape(2000,)
-        self.V1 = scaler.fit_transform(data['data'][0][0].T.astype(np.float32))
-        self.V2 = scaler.fit_transform(data['data'][0][1].T.astype(np.float32))
-        self.V3 = scaler.fit_transform(data['data'][0][2].T.astype(np.float32))
-        self.V4 = scaler.fit_transform(data['data'][0][3].T.astype(np.float32))
-        self.V5 = scaler.fit_transform(data['data'][0][4].T.astype(np.float32))
-        self.V6 = scaler.fit_transform(data['data'][0][5].T.astype(np.float32))
+        data = scipy.io.loadmat(path + 'uci-digit.mat')
+        self.Y = data['Y'].astype(np.int32).reshape(2000,)
+        self.V1 = scaler.fit_transform(data['X'][0][0].astype(np.float32))
+        self.V2 = scaler.fit_transform(data['X'][1][0].astype(np.float32))
+        self.V3 = scaler.fit_transform(data['X'][2][0].astype(np.float32))
     def __len__(self):
         return 2000
     def __getitem__(self, idx):
         x1 = self.V1[idx]
         x2 = self.V2[idx]
         x3 = self.V3[idx]
-        x4 = self.V4[idx]
-        x5 = self.V5[idx]
-        x6 = self.V6[idx]
-        return [torch.from_numpy(x1), torch.from_numpy(x2), torch.from_numpy(x3), torch.from_numpy(x4), torch.from_numpy(x5), torch.from_numpy(x6)], self.Y[idx], torch.from_numpy(np.array(idx)).long()
+        return [torch.from_numpy(x1), torch.from_numpy(x2), torch.from_numpy(x3)], self.Y[idx], torch.from_numpy(np.array(idx)).long()
 
 def load_data(dataset):
-    if dataset == "Mfeat":
-        dataset = Mfeat('./data/')
-        dims = [216, 76, 64, 6, 240, 47]
-        view = 6
+    if dataset == 'UCI-digit':
+        dataset = UCI_digit('./data/')
+        dims = [64, 76, 216]
+        view = 3
         data_size = 2000
         class_num = 10
 
