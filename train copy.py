a1c2572899479d7e5dd3b2038b13c9a6ecf2af97
Yuanyang Zhang <85085067+zhangyuanyang21@users.noreply.github.com>
2025-06-19 18:12:20 +0800

    Add files via upload

---
+++ train copy.py
@@ -0,0 +1,124 @@
+import torch
+from network4 import Network
+from metric import valid
+from metric import evaluate
+from torch.utils.data import Dataset
+import numpy as np
+import argparse
+import random
+from loss import Loss
+from dataloader import load_data
+import os
+import matplotlib.pyplot as plt
+import time
+from torchsummary import summary
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+Dataname = 'UCI-digit'
+parser = argparse.ArgumentParser(description='train')
+parser.add_argument('--dataset', default=Dataname)
+parser.add_argument('--batch_size', default=128, type=int)
+parser.add_argument("--temperature_f", default=0.5)
+parser.add_argument("--temperature_l", default=1)
+parser.add_argument("--learning_rate", default=0.0003)
+parser.add_argument("--weight_decay", default=0.)
+parser.add_argument("--workers", default=8)
+parser.add_argument("--mse_epochs", default=200)
+parser.add_argument("--con_epochs", default=50)
+parser.add_argument("--feature_dim", default=512)
+parser.add_argument("--high_feature_dim", default=128)
+# parser.add_argument("--num_neighbors", default=3)
+args = parser.parse_args()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+if args.dataset == "UCI-digit":
+    args.con_epochs = 100
+    seed = 10
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+setup_seed(seed)
+
+dataset, dims, view, data_size, class_num = load_data(args.dataset)
+
+data_loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        drop_last=True,
+    )
+
+def pretrain(epoch):
+    tot_loss = 0.
+    criterion = torch.nn.MSELoss()
+    for batch_idx, (xs, _, _) in enumerate(data_loader):
+        for v in range(view):
+            xs[v] = xs[v].to(device)
+        optimizer.zero_grad()
+        xrs, zs, hs, qs = model(xs)
+        loss_list = []
+        for v in range(view):
+            loss_list.append(criterion(xs[v], xrs[v]))
+            # loss_list.append(math.log(1+criterion(xs[v], xrs[v])/2**2)/view)
+        loss = sum(loss_list)
+        # loss = torch.tensor(loss, requires_grad=True)  # 将标量转换为张量，并允许计算梯度
+        loss.backward()
+        optimizer.step()
+        tot_loss += loss.item()
+    print('Epoch {}'.format(epoch), 'Loss:{:.6f}'.format(tot_loss / len(data_loader)))
+
+def contrastive_train(epoch):
+    tot_loss = 0.
+    mes = torch.nn.MSELoss()
+    for batch_idx, (xs, y, _) in enumerate(data_loader):
+        for v in range(view):
+            xs[v] = xs[v].to(device)
+        optimizer.zero_grad()
+        xrs, zs, hs, qs = model(xs)
+        _, commonz, commonq = model.MS2FA(xs)
+        loss_list = []
+        for v in range(view):
+            loss_list.append(1*criterion.forward_feature_original(hs[v], commonz))
+            loss_list.append(1*criterion.forward_label(qs[v], commonq))
+        loss = sum(loss_list)
+        loss.backward()
+        optimizer.step()
+        tot_loss += loss.item()
+    print('Epoch {}'.format(epoch), 'Loss:{:.6f}'.format(tot_loss/len(data_loader)))
+
+
+if not os.path.exists('./models'):
+    os.makedirs('./models')
+start_time = time.time()
+T = 1
+for i in range(T):
+    model = Network(view, dims, args.feature_dim, args.high_feature_dim, class_num, device)
+    print(model)
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+    criterion = Loss(args.batch_size, class_num, args.temperature_f, args.temperature_l, device).to(device)
+    epoch = 1
+    while epoch <= args.mse_epochs:
+        pretrain(epoch)
+        epoch += 1
+    while epoch <= args.mse_epochs + args.con_epochs:
+        loss_= contrastive_train(epoch)
+        if epoch == args.mse_epochs + args.con_epochs:
+            acc, nmi, pur, _, _, _, _ = valid(model, device, dataset, view, data_size, class_num, eval_h=True)
+            print('---------train over---------')
+            print('{:.4f} {:.4f} {:.4f}'.format(acc, nmi, pur))
+            state = model.state_dict()
+            torch.save(state, './models/' + args.dataset + '.pth')
+            print('Saving..')
+        epoch += 1
+
+
+
+
+
